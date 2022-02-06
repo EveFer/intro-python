1405a80663b9c908e45b9c8f1a3f573b45d1052b
Fernanda Palacios <fernandapalacios.dev@gmail.com>
2022-02-05 18:35:32 -0600

    virtual environments in python

---
+++ 03-entornos-virtuales/notes.py
@@ -0,0 +1,21 @@
+# source
+# https://github.com/LaunchX-InnovaccionVirtual/CursoIntroPython/blob/main/M%C3%B3dulo%202%20-%20Crear%20y%20administrar%20proyectos/M%C3%B3dulo%202%20-%20Crear%20y%20administrar%20proyectos.md
+
+# El flujo de trabajo basico se ve asi:
+
+# 1. Crear un entorno virtual que no afecte al resto de la máquina.
+# 2. Ingresar al entorno virtual, donde especifica la versión de Python y las bibliotecas que necesita.
+# 3. Desarrolla tu programa.
+
+# Crear un entorno
+# python3 -m venv <name_env>
+
+# Activar entorno
+# source <name_env>/bin/activate
+
+# Para ver paquetes instalados en el entorno
+# pip3 freeze
+
+# Desactivar un entorno
+
+# deactivate
